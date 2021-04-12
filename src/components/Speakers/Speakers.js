0f5452c3bf7ac39e95703ece653fba49dcec6e49
mic <micmor.m@gmail.com>
2021-04-11 20:46:05 -0400

    speakers using render-props

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,18 +1,27 @@
 import React from "react";
-import withData from "./withData";
+import SpeakersRenderProps from "./SpeakersRenderProps";
 
-const Speakers = ({ speakers }) => {
+const Speakers = () => {
   return (
-    <div>
-      {speakers.map(({ imageSrc, name }) => {
+    // Render Prop
+    <SpeakersRenderProps>
+      {({ speakers }) => {
         return (
-          <img src={`images/${imageSrc}.png`} alt={name} key={imageSrc}></img>
+          <div>
+            {speakers.map(({ imageSrc, name }) => {
+              return (
+                <img
+                  src={`images/${imageSrc}.png`}
+                  alt={name}
+                  key={imageSrc}
+                ></img>
+              );
+            })}
+          </div>
         );
-      })}
-    </div>
+      }}
+    </SpeakersRenderProps>
   );
 };
 
-//limit number of speakers
-const maxSpeakersToShow = 2;
-export default withData(maxSpeakersToShow)(Speakers);
+export default Speakers;
