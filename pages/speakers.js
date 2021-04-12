99060bab65cfc5c005711b55a4dc9b90c1d8d423
mic <micmor.m@gmail.com>
2021-04-11 22:56:21 -0400

    add styles

--- pages/speakers.js
+++ pages/speakers.js
@@ -1,17 +1,10 @@
-import Header from "../src/components/Header/Header";
-import Menu from "../src/components/Menu/Menu";
-import SpeakerSearchBar from "../src/components/SpeakerSearchBar/SpeakerSearchBar";
 import Speakers from "../src/components/Speakers/Speakers";
-import Footer from "../src/components/Footer/Footer";
+import Layout from "../src/components/Layout/Layout";
 
 export default function Page() {
   return (
-    <div>
-      <Header />
-      <Menu />
-      <SpeakerSearchBar />
+    <Layout>
       <Speakers />
-      <Footer />
-    </div>
+    </Layout>
   );
 }
